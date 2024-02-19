4c7ec233b92a9c015782716787c107ceae9dbaab
sharpaccount <vladikinastadiki@gmail.com>
2024-02-20 00:57:21 +0300

    added roots finding method and some tests

--- ConsoleApp/Matrix.cs
+++ ConsoleApp/Matrix.cs
@@ -5,16 +5,17 @@ namespace TestMatrix.Utils
     public class Matrix
     {
 
-        private sbyte _CountRows, _CountCols;
-        private double[,] _Matrix;
+        private int _CountRows, _CountCols;
+        private double[,]? _Matrix;
         Random _Random = new Random();
-        public sbyte CountRows
+        
+        public int CountRows
         {
             get => _CountRows;
             set => _CountRows = value;
         }
 
-        public sbyte CountCols
+        public int CountCols
         {
             get => _CountCols;
             set => _CountCols = value;
@@ -23,9 +24,9 @@ namespace TestMatrix.Utils
         public override string ToString()
         {
             StringBuilder builderMatrix = new StringBuilder();
-            for (int i = 0; i < _Matrix.GetUpperBound(0); i++)
+            for (int i = 0; i < _Matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < _Matrix.GetUpperBound(1); j++)
+                for (int j = 0; j < _Matrix.GetLength(1); j++)
                 {
                     builderMatrix.Append(_Matrix[i, j] + " ");
                 }
@@ -37,24 +38,36 @@ namespace TestMatrix.Utils
         public Matrix(double[,] Matrix)
         {
             _Matrix = Matrix;
+            CountRows = _Matrix.GetLength(0);
+            CountCols = _Matrix.GetLength(1);
         }
 
-        public Matrix(sbyte rows, sbyte columns)
+        public Matrix(int rows, int columns)
         {
+            double[,] result = new double[rows,columns];
+
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    _Matrix[i, j] = _Random.NextDouble();
+                    result[i, j] = _Random.NextDouble();
                 }
             }
+            _Matrix = result;
+            CountRows = rows;
+            CountCols = columns;
         }
         public static Matrix MultiplyBy(Matrix matrix1, Matrix matrix2)
         {
-            Matrix result = new Matrix(matrix1.CountRows, matrix2.CountCols);
+            if (matrix1.CountCols != matrix2.CountRows) 
+            {
+                throw new ArithmeticException("Не равное количество строк и столбцов!");
+            }
+            
+            Matrix result = new Matrix(matrix1.CountCols, matrix2.CountRows);
 
-            for (int i = 0; i < result.CountRows; i++)
-                for (int j = 0; j < result.CountCols; j++)
+            for (int i = 0; i < result._Matrix.GetLength(0); i++)
+                for (int j = 0; j < result._Matrix.GetLength(1); j++)
                     result._Matrix[i, j] = Enumerable.Range(0, matrix1.CountCols)
                         .Select(k => matrix1._Matrix[i, k] * matrix2._Matrix[k, j])
                         .Sum();
@@ -69,9 +82,9 @@ namespace TestMatrix.Utils
                 throw new ArithmeticException("Не равное количество строк и столбцов!");
             }
             Matrix result = new Matrix(matrix1.CountRows, matrix1.CountCols);
-            for(int i = 0; i < result._CountRows; i++)
+            for(int i = 0; i < result._Matrix.GetLength(0); i++)
             {
-                for(int j = 0; j < result._CountCols; i++)
+                for(int j = 0; j < result._Matrix.GetLength(1); j++)
                 {
                     result._Matrix[i, j] = matrix1._Matrix[i, j] + matrix2._Matrix[i, j];
                 }
@@ -80,11 +93,11 @@ namespace TestMatrix.Utils
         }
 
         public static double Determinal(Matrix matrix) //void
-        {
+        {   
             int rows = matrix.CountRows;
             int cols = matrix.CountCols;
 
-            matrix = matrix.ToTriangularForm(matrix);
+            matrix = Matrix.ToTriangleForm(matrix);
 
             if (rows != cols)
             {
@@ -97,28 +110,73 @@ namespace TestMatrix.Utils
             {
                 determinant *= matrix._Matrix[i, i];
             }
-
-            return determinant;
+            
+            return Math.Round(determinant, 2);
         }
-
-        private Matrix ToTriangularForm(Matrix matrix)
+        
+        static public Matrix ToTriangleForm(Matrix matrix)
         {
             int rows = matrix.CountRows;
             int cols = matrix.CountCols;
 
-            for (int k = 0; k < cols - 1; k++)
+            for (int i = 0; i < Math.Min(rows, cols); i++)
             {
-                for (int i = k + 1; i < rows; i++)
+                int pivotRow = i;
+                while (pivotRow < rows && matrix._Matrix[pivotRow, i] == 0)
                 {
-                    double factor = matrix._Matrix[i, k] / matrix._Matrix[k, k];
+                    pivotRow++;
+                }
 
-                    for (int j = k; j < cols; j++)
+                if (pivotRow == rows)
+                {
+                    continue;
+                }
+
+                for (int k = 0; k < cols; k++)
+                {
+                    double temp = matrix._Matrix[i, k];
+                    matrix._Matrix[i, k] = matrix._Matrix[pivotRow, k];
+                    matrix._Matrix[pivotRow, k] = temp;
+                }
+
+                for (int j = i + 1; j < rows; j++)
+                {
+                    double factor = matrix._Matrix[j, i] / matrix._Matrix[i, i];
+                    for (int k = i; k < cols; k++)
                     {
-                        matrix._Matrix[i, j] -= factor * matrix._Matrix[k, j];
+                        matrix._Matrix[j, k] -= factor * matrix._Matrix[i, k];
+                        matrix._Matrix[j, k] = Math.Round(matrix._Matrix[j, k], 2);
                     }
                 }
             }
+
             return matrix;
+        }
+
+        static public double[] findRoots(Matrix equationSystem)
+        {
+            if (equationSystem.CountCols - equationSystem.CountRows != 1)
+            {
+                throw new ArithmeticException("Матрица не соответствует стандарту системы уравнений!");
+            }
+
+            int rows = equationSystem.CountRows;
+            int cols = equationSystem.CountCols;
+            
+            double[] roots = new double[cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    sum += equationSystem._Matrix[i, j] * roots[j];
+                }
+
+                roots[i] = Math.Round((equationSystem._Matrix[i, cols - 1] - sum) / equationSystem._Matrix[i, i], 2);
+            }
+
+            return roots;
         }
     }
 }
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using TestMatrix.Utils;
 
 namespace TestProject;
@@ -6,20 +7,199 @@ namespace TestProject;
 public class UnitTest1
 {
     private Matrix _matrix;
+    private Matrix _secMatrix;
 
-    [TestMethod]
-    [DataRow ((sbyte)10, (sbyte)10, (sbyte)15, (sbyte)10)]
-    public void TestMethod1(sbyte rows1, sbyte cols1, sbyte rows2, sbyte cols2)
+    static IEnumerable<object[]> arr1() {
+        return new[] {
+            new []{ new[,] { 
+                { 3.0, 2.0, 1.0 },
+                { 3.0, 2.0, 6.0 },
+                { 3.0, 2.0, 5.0 } } }
+        };
+    }
+    
+    static IEnumerable<object[]> arr2() {
+        return new[] {
+            new []{ new[,]
+            {
+                { 2.5, 2.0, 1.0 },
+                { 2.5, 2.0, 1.0 },
+                { 2.5, 2.0, 1.0 },
+                { 2.5, 3.0, 1.0 }
+            } }
+        };
+    }
+    
+    static IEnumerable<object[]> arr3() {
+        return new[] {
+            new []{ new[,]
+            {
+                { 2.5, 2.0, 1.0, 2.2 },
+                { 2.5, 2.3, 9.0, 5.2 },
+                { 2.0, 2.0, 1.6, 1.5 }
+            } }
+        };
+    }
+    
+    static IEnumerable<object[]> arr4() {
+        return new[] {
+            new []{ new[,]
+            {
+                { 2.5, 2.0 },
+                { 2.5, 2.0 }
+            } }
+        };
+    }
+    
+    static IEnumerable<object[]> arr5() {
+        return new[] {
+            new []{ new[,] { 
+                { 3.0, 2.0, 1.0, 6.5 },
+                { 3.0, 2.0, 6.0, 8.3 },
+                { 3.0, 2.0, 5.0, 10.0 },
+                { 1.0, 3.1, 5.5, 1.0 }
+            } }
+        };
+    }
+
+    [TestMethod]   
+    [DataRow (10, 10)]
+    [DataRow (20, 20)]
+    [DataRow (15, 10)]
+    [DataRow (30, 30)]
+
+    public void areRowsAndColumnsEqual(int rows1, int cols1)
     {
         _matrix = new Matrix(rows1, cols1);
         Assert.IsTrue(_matrix.CountCols == cols1 && _matrix.CountRows == rows1);
-                _matrix = new Matrix(rows2, cols2);
-        Assert.IsTrue(_matrix.CountCols == cols2 && _matrix.CountRows == rows2);
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(arr1), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr2), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr3), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr4), DynamicDataSourceType.Method)]
+    
+    public void IsArrayCorrect(double[,] arr)
+    {
+        _matrix = new Matrix(arr);
+        Console.WriteLine(_matrix.ToString());
     }
 
-
-    public void TestMethod2(double[,] testMatrix)
+    [TestMethod] 
+    [DataRow (10, 10)]
+    [DataRow (20, 20)]  
+    [DataRow (3, 3)]  
+    public void showMatrix(int rows1, int cols1)
     {
+        _matrix = new Matrix(rows1, cols1);
+        Console.WriteLine(_matrix.ToString());   
+    }
+
+    [TestMethod] 
+    [DataRow (10, 10, 8, 8)]
+    [DataRow (8, 10, 7, 7)]  
+    [DataRow (11, 11, 6, 6)]  
+
+    public void areRowsAndColumnsNotEqualInSum(int rows1, int cols1, int rows2, int cols2)
+    {
+        _matrix = new Matrix(rows1, cols1);
+        _secMatrix = new Matrix(rows2, cols2);
+
+        Matrix result = Matrix.SumBy(_matrix, _secMatrix);
+    }
+
+    [TestMethod] 
+    [DataRow (8, 8)]
+    [DataRow (3, 3)]  
+
+    public void SumMatrices(int rows1, int cols1)
+    {
+        _matrix = new Matrix(rows1, cols1);
+        _secMatrix = new Matrix(rows1, cols1);
+
+        Matrix result = Matrix.SumBy(_matrix, _secMatrix);
+        Console.WriteLine(result.ToString());   
+    }
+    
+    [TestMethod] 
+    [DataRow (10, 10, 8, 8)]
+    [DataRow (8, 10, 7, 7)]  
+
+    public void areRowsAndColumnsNotEqualInMultiply(int rows1, int cols1, int rows2, int cols2)
+    {
+        _matrix = new Matrix(rows1, cols1);
+        _secMatrix = new Matrix(rows2, cols2);
+
+        Matrix result = Matrix.MultiplyBy(_matrix, _secMatrix);
+    }
+    
+    [TestMethod] 
+    [DataRow (10, 10, 10, 10)]
+    [DataRow (7, 7, 7, 7)]  
+
+    public void areResultRowsEqualToMMatrixultiply(int rows1, int cols1, int rows2, int cols2)
+    {
+        _matrix = new Matrix(rows1, cols1);
+        _secMatrix = new Matrix(rows2, cols2);
+
+        Matrix result = Matrix.MultiplyBy(_matrix, _secMatrix);
+        Assert.IsTrue(result.CountRows == _matrix.CountCols && result.CountCols == _matrix.CountRows);
+    }
+    
+    [TestMethod] 
+    [DataRow (10, 10, 8, 8)]
+    [DataRow (8, 10, 7, 7)]  
+    [DataRow (8, 7, 7, 8)]
+
+    public void Multiply(int rows1, int cols1, int rows2, int cols2)
+    {
+        _matrix = new Matrix(rows1, cols1);
+        _secMatrix = new Matrix(rows2, cols2);
+
+        Matrix result = Matrix.MultiplyBy(_matrix, _secMatrix);
+        Console.WriteLine(result.ToString());
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(arr1), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr5), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr3), DynamicDataSourceType.Method)]
+
+
+    public void transformToTriangleForm(double[,] matrix)
+    {
+        Matrix matrixToTransform = new Matrix(matrix);
+        Matrix.ToTriangleForm(matrixToTransform);
         
+        Console.WriteLine(matrixToTransform.ToString());
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(arr1), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr5), DynamicDataSourceType.Method)]
+
+    public void findDeterminal(double[,] matrix)
+    {
+        Matrix matrixToTransform = new Matrix(matrix);
+        double determinal = Matrix.Determinal(matrixToTransform);
+        
+        Console.WriteLine(determinal);
+    }
+    
+    [TestMethod]
+    [DynamicData(nameof(arr3), DynamicDataSourceType.Method)]
+    [DynamicData(nameof(arr5), DynamicDataSourceType.Method)]
+
+    public void findRoots(double[,] matrix)
+    {
+        Matrix matrixToFind = new Matrix(matrix);
+        matrixToFind = Matrix.ToTriangleForm(matrixToFind);
+
+        double[] roots = Matrix.findRoots(matrixToFind);
+        foreach (double root in roots)
+        {
+            Console.Write(root + " ");
+        }
     }
 }
